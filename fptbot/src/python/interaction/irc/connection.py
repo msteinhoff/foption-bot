d8316e369ad891ff29397bea0e13d511b5a27693
Mario Steinhoff <steinhoff.mario@gmail.com>
2011-01-07 12:04:53 +0000

    refactoring with python coding style

--- fptbot/src/python/interaction/irc/connection.py
+++ fptbot/src/python/interaction/irc/connection.py
@@ -1,13 +1,31 @@
-'''
-Created on Jan 6, 2011
+"""
+$Header$
 
-@author: msteinhoff
+Copyright (c) 2010 foption
 
-low level network connection
-'''
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+from socket import socket
 
 from core.messages import message
-from socket import socket
+
 
 class Connection( object ):
     """
@@ -17,37 +35,38 @@ class Connection( object ):
     
     It handles data sending and receiving and adds/strips required
     control characters like \r or \n where appropriate
+    
+    please note that this implementation currently does not support SSL encrypted connections
     """
     
-    ' the socket connection '
-    socket = None
     
-    ' the timeout after the connection is closed '
-    timeout = 180
-    
-    ' the receive buffer of the socket '
-    recvBuffer = 4096
-
-    ' determines whether the socket is opened '
-    isOpened = False
-    
-
-    def __init__(self, timeout, buffer):
+    def __init__(self, timeout=180, recvBuffer=4096):
         """
         initialize connection parameters
+        
+        @param timeout:    the socket timeout in seconds
+        @param recvBuffer: the socket receive buffer in bytes
         """
         
-        if timeout != None:
-            self.timeout = timeout
-            
-        if buffer != None:
-            self.recvBuffer = buffer
+        self.timeout    = timeout
+        self.recvBuffer = recvBuffer
+        self.isOpened   = False
 
 
     def open(self, server):
         """
-        opens and connects the socket
+        open and connect the socket
+        
+        @param server: a dictionary with server connection data
+        
+                       the dictionary should contain the following keys:
+                       
+                       server.address: An IP address or DNS name
+                       server.port:    The port
         """
+        
+        if server.address == None or server.port == None:
+            raise ValueError("invalid server data")
         
         try:
             log.info(message[20001], {'address': server.address, 'port': server.port})
@@ -61,25 +80,13 @@ class Connection( object ):
             # todo configure blocking/non-blocking
             
             # set internal state
-            self.socketOpened = True
+            self.isOpened = True
                
         except:
             #TODO checken welche exceptions hier runterkommen und entsprechendes fehlerhandling    
             log.error(message[20002])
 
         
-    def close(self):
-        """
-        closes the socket
-        """
-        log.info(message[20006])
-        
-        self.socket.close()
-        self.socket = None
-        
-        self.isSocketOpened = False
-        
-    
     def send(self, data):
         """
         write data into the socket
@@ -87,7 +94,7 @@ class Connection( object ):
         @param data: the data to send, without any command characters (like \r or \n)
         """
         
-        if not self.isSocketOpened:
+        if not self.isOpened:
             raise IOError("the socket is not open")
         
         try:
@@ -101,14 +108,14 @@ class Connection( object ):
         read data from the socket
         """
         
-        if not self.isSocketOpened:
+        if not self.isOpened:
             raise IOError("the socket is not open")
         
         try:
-            data = self.socket.recv(self.socketRecvBuffer);
+            data = self.socket.recv(self.recvBuffer);
             
         except socket.timeout:
-            log.error(message[20004], {'seconds': self.socketTimeout})
+            log.error(message[20004], {'seconds': self.timeout})
             
             self.close()
             return
@@ -123,4 +130,17 @@ class Connection( object ):
         data.replace('\r', '')
         
         return data
-    
+
+
+    def close(self):
+        """
+        close the socket
+        """
+        
+        log.info(message[20006])
+        
+        self.socket.close()
+        self.socket = None
+        
+        self.isOpened = False
+
