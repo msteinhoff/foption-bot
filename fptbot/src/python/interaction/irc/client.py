5b337c39eb571650d57da9c452ded494c67c505a
Mario Steinhoff <steinhoff.mario@gmail.com>
2011-01-13 22:14:04 +0000

    first working implementation using python's asyncore

--- fptbot/src/python/interaction/irc/client.py
+++ fptbot/src/python/interaction/irc/client.py
@@ -27,298 +27,259 @@ THE SOFTWARE.
 @author Mario Steinhoff
 """
 
+from string   import split, join
+from socket   import AF_INET
+from socket   import SOCK_STREAM
+from asyncore import loop
+from asynchat import async_chat
+
 from core.messages              import message
-from interaction.irc.connection import Connection
+from core.config                import Config
+from interaction.interaction    import Interaction
+from interaction.irc.user       import Userlist
+from interaction.irc.user       import User
 
-class Client( object ):
-    """
-    A IRC client implementation according to RFC 2812
+SPACE      = "\x20"
+CRLF       = "\x0D\x0A"
+MIRC_COLOR = "\x03"
 
-    this list describes which messages are implemented: 
-       3.  Message Details ............................................   9
-          3.1  Connection Registration ................................  10
-             3.1.1  Password message ..................................  10    - not needed
-             3.1.2  Nick message ......................................  10    - needed
-             3.1.3  User message ......................................  11    - needed
-             3.1.4  Oper message ......................................  12    - not needed
-             3.1.5  User mode message .................................  12    - needed
-             3.1.6  Service message ...................................  13    - not needed
-             3.1.7  Quit ..............................................  14    - needed
-             3.1.8  Squit .............................................  15    - not needed
-          3.2  Channel operations .....................................  15
-             3.2.1  Join message ......................................  16    - needed
-             3.2.2  Part message ......................................  17    - needed
-             3.2.3  Channel mode message ..............................  18    - needed
-             3.2.4  Topic message .....................................  19    - needed
-             3.2.5  Names message .....................................  20    - needed
-             3.2.6  List message ......................................  21    - not needed
-             3.2.7  Invite message ....................................  21    - not needed (maybe implemented in the future)
-             3.2.8  Kick command ......................................  22    - needed
-          3.3  Sending messages .......................................  23
-             3.3.1  Private messages ..................................  23    - needed
-             3.3.2  Notice ............................................  24    - needed
-          3.4  Server queries and commands ............................  25
-             3.4.1  Motd message ......................................  25    - needed
-             3.4.2  Lusers message ....................................  25    - not needed
-             3.4.3  Version message ...................................  26    - not needed
-             3.4.4  Stats message .....................................  26    - not needed
-             3.4.5  Links message .....................................  27    - not needed
-             3.4.6  Time message ......................................  28    - not needed
-             3.4.7  Connect message ...................................  28    - not needed
-             3.4.8  Trace message .....................................  29    - not needed
-             3.4.9  Admin command .....................................  30    - not needed
-             3.4.10 Info command ......................................  31    - not needed
-          3.5  Service Query and Commands .............................  31
-             3.5.1  Servlist message ..................................  31    - not needed
-             3.5.2  Squery ............................................  32    - not needed
-          3.6  User based queries .....................................  32
-             3.6.1  Who query .........................................  32    - needed
-             3.6.2  Whois query .......................................  33    - needed
-             3.6.3  Whowas ............................................  34    - not needed
-          3.7  Miscellaneous messages .................................  34
-             3.7.1  Kill message ......................................  35    - not needed
-             3.7.2  Ping message ......................................  36    - needed
-             3.7.3  Pong message ......................................  37    - needed
-             3.7.4  Error .............................................  37    - not needed
-     """
-    
-    
+class Client(Interaction, async_chat):
     """
-    User data
-    
-    This dictionary should contain the following keys:
+    Provide a RFC 2812 compilant IRC client implementation using asyncore.
     """
-    user = {}
     
-    """
-    Connection data
-    """
-    server = {}
-    
-    def __init__(self, user, server):
-        """
-        the constructor
-        
-        @param user:   A dictionary with user information, should contain the following keys:
-                       nickname
-                       realname
-                       ident
-                       
-        @param server: a dictionary with server connection data, should contain the following keys:
-                       address: An IP address or DNS name
-                       port:    The port
-        """
-
-        """
-        self.nick = "foption"
-        self.anick = "fptbot"
-        self.cnick = "foption"
-        self.Ident = "foption"
-        self.Realname = "teh slut"
-        self.Channel = "#foption"
-        self.Server = "irc.quakenet.org"
-        self.Port = 6668
-        self.admin = ["rack","whg|abc","Marrek","diggen"]
-        """
-
-        if user == None:
-            raise ValueError("user data missing")
-        
-        if server == None:
-            raise ValueError("server data missing")
-        
-        self.user   = user
-        self.server = server
-        
-        self.connection  = connection.Connection()
-        self.isConnected = False
-        self.terminateConnection = False
-
-    
-    def connect(self):
-        '''
-        open the irc connection
-        '''
-        
-        while True:
-            # open socket
+    class ClientConfig(Config):
+        def __init__(self, persistence):
+            Config.__init__(self, "irc-client", persistence);
             
-            isStreamOpened = True
-            
-            while isStreaming:
-                if self.terminateConnection:
-                    log.info(message[20003])    
+        def valid(self):
+            return [
+                "nickname",
+                "anickname",
+                "realname",
+                "ident",
+                "address",
+                "port"
+            ]
+        
+        def defaults(self):
+            return {
+                "nickname"  : "bot-test",
+                "anickname" : "bot-test-",
+                "realname"  : "bot",
+                "ident"     : "bot",
+                "address"   : "de.quakenet.org",
+                "port"      : 6667
+            }
 
-                    # close socket                                      
-                    break
-                    
-                data = ""
-            
-                #receive
-                #parse data and dispatch to listener methods functions
-                    
-                    #print str(rawmsg)
-                    
-                    for i in Conf.Modlist:
-                        try:
-                            r = re.findall(i.regexpattern,rawmsg)
-                            if (r):
-                                i.handleInput(r[0])
-                        except:
-                            ServerSocket.send("PRIVMSG " + Conf.Channel \
-                            + " :4Error in '" + str(i) + "' --> '" \
-                            + str(sys.exc_info()[1]) + "'\n")
+    def __init__(self, bot):
+        """
+        Initialize the IRC client.
+        
+        @param bot: The bot instance that this client should use.
+        """
+        
+        Interaction.__init__(self, bot)
+        async_chat.__init__(self)
+        
+        self._logger = self._bot.getLogger("interaction.irc")
+        self._config = self.ClientConfig(bot.getPersistence())
+        self._userlist = Userlist()
+        self._modules = {}
+        
+        self._isConnected = False
+        
+        self.set_terminator(CRLF)
 
-
-            if (Conf.Run == False):
-                break
-
-
+    def register_module(self, name):
+        """
+        Load a module that extends the client's functionality.
+        
+        @param name: The name of the module that should be registered
+        """
+        
+        moduleName = "modules.%s" % name
+        
+        module = __import__(moduleName, globals(), locals(), [], -1)
+        
+        clazz = getattr(module, module.moduleName)
+        
+        self._modules[name] = clazz()
+        
+    def get_module(self, name):
+        """
+        Return a given module instance.
+        
+        @param name: The name of the module that should be returned.
+        @return The module instance, a subclass of Module.
+        @raise KeyError if there is no such module 
+        """
+        
+        return self._modules[name]
     
-    for i in Conf.Modlist:
+    """-------------------------------------------------------------------------
+    Connection handling
+    -------------------------------------------------------------------------"""
+    def start(self):
+        """
+        Connect to the IRC server.
+        """
+        
+        self._logger.info(message[20001], {
+            'address' : self._config.get('address'),
+            'port'    : self._config.get('port')
+        })
+        
         try:
-            i.shutdown()
+            self.create_socket(AF_INET, SOCK_STREAM)
+            self.connect((self._config.get('address'), self._config.get('port')))
+            
+            self._buffer = []
+            
+            loop()
         except:
-            pass
-    
-    def disconnect(self):
-        '''
-        aaa
-        '''
+            #TODO: check which exceptions are caught here
+            self._logger.info.error(message[20002])
+
+    def stop(self):
+        """
+        Disconnect from the IRC server.
+        """
         
-        pass
+        self._logger.info(message[20003])
+        self.close_when_done()
 
-    def __registerConnection(self):
-        '''
-        aaa
-        '''
+    """-------------------------------------------------------------------------
+    Communication interface
+    
+    TODO: Create consistent API to send/compose and receive/parse IRC messages.
+    -------------------------------------------------------------------------"""
+    def receive_irc(self, data):
+        """
+        Handle all interaction when there is protocol data received.
         
-        pass
-
-    def __msgRegistrationNick(self, nickname):
-        '''
-        aaa
-        '''
+        Strip IRC protocol control characters.
         
-        self.socket.send("NICK %s\r\n" % nickname)
-    
-    def __msgRegistrationUser(self, ident, mode, realname):
-        '''
-        aaa
-        '''
+        @param data: The data which was received.
+        """
         
-        self.socket.send("USER %s %d %s :%s\r\n" % (ident mode, realname))
-    
-    def __msgRegistrationMode(self, nickname, mode):
-        '''
-        aaa
-        '''
+        data = [line.strip(CRLF) for line in data]
         
-        self.socket.send("MODE %s %s", % (nickname, mode))
+        source, command, params = self.parse_message(data)
         
-    
-    def __msgRegistrationQuit(self):
+        self._logger.info("Received: <%s> %s: %s (Raw: %s)" % (source, command, params, data))
         
-        '''
-        aaa
-        '''
+        if command == 'PING':
+            self.send_irc(["PONG", params[0]])
+            
+        if command == '001':
+            self.send_irc(["JOIN", "#botbot.test"])
+
+    def send_irc(self, data):
+        """
+        Handle all interaction when there is protocol data to send.
         
-        pass
-    
-    def __msgChannelJoin(self):
-        '''
-        aaa
-        '''
+        Add IRC protocol control characters.
         
-        pass
-    
-    def __msgChannelPart(self):
-        '''
-        aaa
-        '''
-
-        pass
-    
-    def __msgChannelMode(self):
-        '''
-        aaa
-        '''
+        @param data: The data to send.
+        """
         
-        pass
-    
-    def __msgChannelTopic(self):
-        '''
-        aaa
-        '''
+        self._logger.info("Sent:     %s" % join(data))
+        self.push("%s%s" % (join(data), CRLF))
 
-        pass
-    
-    def __msgChannelNames(self):
-        '''
-        aaa
-        '''
+    """-------------------------------------------------------------------------
+    Message handling
+    -------------------------------------------------------------------------"""
+    def parse_message(self, message):
+        """
+        Parse an incoming IRC message.
+        
+        The message is parsed into the following components:
+        - source, a string with the format e.g. nickname!ident@host, or None  
+        - command, the IRC command or reply
+        - params, all parameters, parsed according to RFC section 2.3.1
+        
+        Currently, the implementation is not a real parser, but works
+        in most conditions.
+        
+        TODO: write a proper parser with full RFC support. 
+        
+        @param message: The raw message
+        
+        @return a tupel (source, command, params)
+        """
+        
+        """---------------------------------------------------------------------
+        Handle the source of the message, if existent
+        ---------------------------------------------------------------------"""
+        if message[0] == ':':
+            source, message = split(message[1:], ' ', 1)
+        else:
+            source = None
 
-        pass
-    
-    def __msgChannelKick(self):
-        '''
-        aaa
-        '''
+        """---------------------------------------------------------------------
+        Parse the message components
+        
+        The command and its parameters are separated by the follwing rules:
+        
+        - The command can be any string with at least 1 letter or a 3 digit number.
+        - A command can only have 14 parameters.
+        - Every parameter must start with a space.
+        - After the space, there must be at least one character except NUL, CR, LF, space, or colon.
+        - After the first character, there can be further characters or colons.
+        - If the parameter itself contains a space, it has to be separated by a space and a colon, " :"
+        [...]
+        ---------------------------------------------------------------------"""
+        try:
+            message = split(message, ' :')
+            
+            command = message[0]
+            params = message[1:]
+        
+        except ValueError:
+            command = message
+            params = ''
+        
+        return source, command, params
 
-        pass
+    """-------------------------------------------------------------------------
+    Implementation of asyncore methods 
+    -------------------------------------------------------------------------"""
+    def handle_connect(self):
+        """
+        Implement IRC protocol for connecting and set internal state.
+        """
+        
+        self.send_irc(["NICK", self._config.get('nickname')])
+        self.send_irc(["USER", self._config.get('ident'), "0", "*", ":%s" % self._config.get('realname')])
+        
+        self._isConnected = True
     
-    def __msgMessagePrivmsg(self):
-        '''
-        aaa
-        '''
+    def handle_close(self):
+        """
+        Implement IRC protocol for disconnecting and set internal state.
+        """
+        
+        self.send_irc(["QUIT", "i did it for the lulz mkay"])
+        
+        self._isConnected = False
 
-        pass
-    
-    def __msgMessageNotice(self):
-        '''
-        aaa
-        '''
+    """-------------------------------------------------------------------------
+    Implementation of asynchat methods 
+    -------------------------------------------------------------------------"""
+    def collect_incoming_data(self, data):
+        """
+        Add any received data to the internal buffer.
+        """
+        
+        self._buffer.append(data)
 
-        pass
-    
-    def __msgServerMotd(self):
-        '''
-        aaa
-        '''
-
-        pass
-    
-    def __msgUserWho(self):
-        '''
-        aaa
-        '''
-
-        pass
-    
-    def __msgUserWhois(self):
-        '''
-        aaa
-        '''
-
-        pass
-    
-    def __msgMiscPing(self):
-        '''
-        aaa
-        '''
-
-        pass
-    
-    def __msgMiscPong(self):
-        '''
-        aaa
-        '''
-
-        pass
-    
-    def __msgNick(self):
-        '''
-        aaa
-        '''
-
-        pass
+    def found_terminator(self):
+        """
+        Send event when a command terminator was found.
+        """
+        
+        data = self._buffer
+        self._buffer = []
+        
+        self.receive_irc(data)
