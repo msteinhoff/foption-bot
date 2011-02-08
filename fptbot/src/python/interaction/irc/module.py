fbc155a15dd7dfb581a10929054ff49d99b6cbb1
Mario Steinhoff <steinhoff.mario@gmail.com>
2011-02-08 22:54:37 +0000

    reworked InteractiveModule reworked command definition reworked command handling added ModuleCommand added Location added Role

--- fptbot/src/python/interaction/irc/module.py
+++ fptbot/src/python/interaction/irc/module.py
@@ -31,6 +31,7 @@ __version__ = "$Rev$"
 
 import re
 
+from interaction.irc.message import CHANNEL_TOKEN
 from interaction.irc.command import PrivmsgCmd, NoticeCmd
 
 MIRC_COLOR = "\x03"
@@ -39,9 +40,9 @@ class Module(object):
     """
     Extend the IRC client's functionality.
     
-    This module is loaded and instantiated by the client. It provides
-    an interface the IRC by registering callback functions on
-    any IRC command known by the client.
+    A module is loaded and instantiated by the client. An interface
+    to the IRC is provided by registering callback functions for each
+    IRC command needed and known by the client.
     """
     
     def __init__(self, client):
@@ -60,6 +61,9 @@ class Module(object):
     def __del__(self):
         self.shutdown()
 
+    """-------------------------------------------------------------------------
+    IRC interface 
+    -------------------------------------------------------------------------"""
     def get_receive_listeners(self):
         """
         Return a mapping between commands and callback functions.
@@ -72,10 +76,25 @@ class Module(object):
         """
         raise NotImplementedError
     
+    """-------------------------------------------------------------------------
+    State handling
+    -------------------------------------------------------------------------"""
     def initialize(self):
+        """
+        Initialization hook.
+        
+        This will execute additional initialization code in the module
+        without overriding the constructor.
+        """
         pass
     
     def shutdown(self):
+        """
+        Cleanup hook.
+        
+        This will execute additional cleanup code in the module without
+        overriding the destructor.
+        """
         pass
 
 class InteractiveModule(Module):
@@ -93,8 +112,7 @@ class InteractiveModule(Module):
     Parameters can be defined for commands and sub-commands.
     """
     
-    COMMAND_TRIGGER = '.'
-    PATTERN_BASE = r'^\{0}(?P<command>{1})(\s(?P<parameter>.+)$|$)'
+    PATTERN_BASE = r'^\.(?P<command>{0})(\s(?P<parameter>.+)$|$)'
     PATTERN_SEPARATOR = '|'
     
     def __init__(self, client):
@@ -110,19 +128,33 @@ class InteractiveModule(Module):
         
         Module.__init__(self, client)
         
-        self.command_map = self.command_mapping()
+        # cache reference to userlist
+        self.userlist = self.client.get_module('usermgmt').userlist
         
-        regex = self.PATTERN_BASE.format(
-            self.COMMAND_TRIGGER,
-            self.PATTERN_SEPARATOR.join(self.command_map)
+        self.identifier = self.module_identifier()
+        self.command_dict = {}
+
+        self.init_commands()
+        
+        pattern = self.PATTERN_BASE.format(
+            self.PATTERN_SEPARATOR.join(self.command_dict)
         )
         
-        self.command_re = re.compile(regex, re.I)
-        
-        self.parameter_re = []
-        for command, parameter_pattern in self.parameter_mapping().items():
-            self.parameter_re[command] = re.compile(parameter_pattern, re.I)
+        self.module_re = re.compile(pattern, re.I)
     
+    """-------------------------------------------------------------------------
+    Overridden methods
+    -------------------------------------------------------------------------"""
+    def get_receive_listeners(self):
+        """
+        Setup default receive listeners needed for Privmsg handling.
+        """
+        
+        return {PrivmsgCmd: self.parse}
+
+    """-------------------------------------------------------------------------
+    Identification and behavior
+    -------------------------------------------------------------------------"""
     def module_identifier(self):
         """
         Return a verbose string that identifies the module.
@@ -134,7 +166,7 @@ class InteractiveModule(Module):
         
         raise NotImplementedError
     
-    def command_mapping(self):
+    def init_commands(self):
         """
         Return a definition of commands/sub-commands and callbacks.
         
@@ -145,26 +177,34 @@ class InteractiveModule(Module):
         space (0x20) character.
         
         Examples:
-        {'roll': callback}
-        {'black': callback, 'white': callback, 'pink': callback}
-        {'calendar add': callback, 'calendar delete': callback}
+        self.addCommand('roll', r'^([\d]+)(?:-([\d]+))$', Role.USER, self.roll)
         
-        @return A dictionary with the command/sub-command keywords.
+        self.add_command('black', None, Role.USER, self.choose)
+        self.add_command('white', None, Role.USER, self.choose)
+        self.add_command('pink',  None, Role.USER, self.choose)
+        
+        self.add_command('calendar add',    '???', Role.USER, self.add)
+        self.add_command('calendar delete', '???', Role.USER, self.delete)
+        
+        @return none
         """
         
         raise NotImplementedError
     
-    def parameter_mapping(self):
+    """-------------------------------------------------------------------------
+    Module command handling
+    -------------------------------------------------------------------------"""
+    def add_command(self, keyword, pattern, location, role, callback):
         """
-        Return a dictionary that maps parameters to commands or
-        sub-commands.
+        Add a command to the internal command dictionary.
         
-        Example:
-        {'roll': r'^([\d]+)(?:-([\d]+))$'}
-        {'calendar add': r'^([\d]+)(?:-([\d]+))?$'}
+        @param keyword: The keyword that triggers the command.
+        @param pattern: A regex pattern that defines the arguments.
+        @param location: The location where the command can be executed.
+        @param role: The role the calling user needs to have.
+        @param callback: The callback function.
         """
-        
-        raise NotImplementedError
+        self.command_dict[keyword] = ModuleCommand(self, keyword, pattern, location, role, callback)
     
     def parse(self, event):
         """
@@ -190,32 +230,47 @@ class InteractiveModule(Module):
         if event.command not in (PrivmsgCmd.token(), NoticeCmd.token()):
             return
         
-        message = event.parameter[1]
+        """---------------------------------------------------------------------
+        Handle command
+        ---------------------------------------------------------------------"""
+
+        target, message = event.parameter[0:2]
         
-        message_match = self.command_re.search(message)
+        message_match = self.module_re.search(message)
         
         if message_match is None:
             return
         
         command = message_match.group('command')
+        
+        command_object = self.command_dict[command]
+        
+        # check location and access
+        # TODO: print error message on insufficient privileges?
+        if not command_object.valid_location(target) or \
+           not command_object.check_access(event.source):
+            return
+        
+        """---------------------------------------------------------------------
+        Handle parameter
+        ---------------------------------------------------------------------"""
         parameter = message_match.group('parameter')
-        
+
         try:
-            parameter_match = self.parameter_re[command].findall(parameter)
-            
-            try:
-                parameter = parameter_match[0]
-            except KeyError:
-                self.invalid_parameters(event, command)
-                return
-        
-        except KeyError:
+            callback = command_object.callback
+            parameter = command_object.match_arguments(parameter)
+        except ValueError:
+            callback = self.invalid_parameters
             parameter = None
         
-        callback = self.command_map[command]
-        
+        """---------------------------------------------------------------------
+        Dispatch
+        ---------------------------------------------------------------------"""
+
+        location = Location.get(target)
+
         try:
-            callback(event, command, parameter_match)
+            callback(event, location, command, parameter)
             
         except Exception as ex:
             """
@@ -224,13 +279,44 @@ class InteractiveModule(Module):
             TODO: throw stack trace
             """
             
-            self.client.logger.error('Unhandled exception {1} thrown in {0}'.format(
+            self.client.logger.error('Unhandled exception "{1}" thrown in {0}'.format(
                 self.__class__.__name__,
                 str(ex)
             ))
     
-    def invalid_parameters(self, event, command):
+    def invalid_parameters(self, event, command, parameter):
+        """
+        Handle invalid argument errors.
+        
+        @param command: The command keyword.
+        @param parameter: The raw parameter data.
+        """
+        
         pass
+    
+    def get_target(self, location_from, event):
+        """
+        Based on the location, extract the target for the reply.
+        
+        TODO: handle reply in parse, make reply the return string or None for no reply
+        
+        @param location_from: The location value (CHANNEL or QUERY)
+        @param event: The event to analyze.
+        
+        @return The target from the event.
+        
+        @raise ValueError: If the location is invalid.
+        """
+        if location_from == Location.CHANNEL:
+            target = event.parameter[0]
+            
+        elif location_from == Location.QUERY:
+            target = event.source.nickname
+            
+        else:
+            raise ValueError("location_from may only be channel or query")
+        
+        return target
     
     def send_reply(self, command, target, reply):
         """
@@ -251,6 +337,125 @@ class InteractiveModule(Module):
         if command.token() not in (PrivmsgCmd.token(), NoticeCmd.token()):
             raise ValueError('only Privmsg or Notice are valid commands')
         
-        reply = '{}: {}'.format(self.module_identifier(), reply)
+        reply = '{0}: {1}'.format(self.identifier, reply)
         
         self.client.send_command(command, target, reply)
+
+class ModuleCommand(object):
+    """
+    Represent a module command that can be triggered by IRC users.
+    """
+    
+    def __init__(self, module, keyword, pattern, location, role, callback):
+        """
+        Initialize the command.
+        
+        @param module: The module instance the command belongs to.
+        @param keyword: The keyword that triggers the command.
+        @param pattern: A regex pattern that defines the arguments.
+        @param role: The role the calling user needs to have.
+        @param callback: The callback function.
+        """
+        
+        self.module = module
+        self.keyword = keyword
+        
+        if pattern:
+            self.pattern = re.compile(pattern, re.I)
+        else:
+            self.pattern = None
+        
+        self.location = location
+        self.role = role
+        self.callback = callback
+        
+    def valid_location(self, target):
+        """
+        Determine whether the command can be executed in the location.
+        
+        @param target: The location to check against.
+        """
+        
+        return Location.valid(self.location, Location.get(target))
+    
+    def check_access(self, source):
+        """
+        Check if the given source has sufficient access privileges.
+        """
+        
+        user = self.module.userlist.get(source)
+        role = user.getInfo('Privileges')
+        
+        return Role.valid(self.role, role)
+        
+    def match_arguments(self, data):
+        """
+        Match and extract data according to the object's pattern.
+        
+        @param data: A string.
+        
+        @return: A tuple, the format depends on the pattern.
+        
+        @raise ValueError If the data is invalid.
+        """
+        
+        if self.pattern is None:
+            return None
+        
+        try:
+            match = self.pattern.findall(data)
+            arguments = match[0]
+        
+        # TypeError when data can not be matched
+        # KeyError when no match was found
+        except TypeError, KeyError:
+            raise ValueError
+        
+        except IndexError:
+            raise ValueError
+
+        return arguments
+    
+class Role(object):
+    """
+    Represent a role neccessary to execute a ModuleCommand.
+    
+    TODO: need real object here or maybe move to own python module?
+    """
+    
+    USER  = 1 # Right.USER
+    ADMIN = 3 # Right.USER | Right.ADMIN
+    
+    def __init__(self):
+        raise NotImplementedError
+    
+    @staticmethod
+    def valid(required, role):
+        return (required & role == required) 
+
+class Location(object):
+    """
+    Represent a location determining where a ModuleCommand can be executed.
+    TODO: need real object here or maybe move to own python module?
+    """
+    
+    CHANNEL = 1
+    QUERY   = 2
+    BOTH    = CHANNEL | QUERY
+
+    def __init__(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def get(target):
+        if target.startswith(CHANNEL_TOKEN):
+            location = Location.CHANNEL
+        else:
+            location = Location.QUERY
+        
+        return location
+
+
+    @staticmethod
+    def valid(required, location):
+        return (required | location == required)
