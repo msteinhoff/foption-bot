0dd7e7b786afa8d467b0ac259e15ba90a93fd0ae
Mario Steinhoff <steinhoff.mario@gmail.com>
2011-03-10 10:33:53 +0000

    initial version

---
+++ fptbot/src/python/persistence/sqlite.py
@@ -0,0 +1,58 @@
+# -*- coding: UTF-8 -*-
+"""
+$Id$
+
+$URL$
+
+Copyright (c) 2011 foption
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+@since Mar 08, 2011
+@author Mario Steinhoff
+"""
+
+__version__ = '$$'
+
+import sqlite3
+
+from persistence import Persistence
+
+class DatabaseError(Exception):
+    pass
+
+class SQLitePersistence(Persistence):
+    """
+    Provide sqlite connectivity to all sub-systems.
+    """
+
+    def __init__(self, filename):
+        self.connection = sqlite3.connect(filename)
+
+    def get_connection(self):
+        """
+        """
+        
+        return self.connection
+    
+    def get_cursor(self):
+        """
+        """
+        
+        return self.connection.cursor()
