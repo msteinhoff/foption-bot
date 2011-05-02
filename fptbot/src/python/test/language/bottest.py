bfabbb6eaa9de431ec0184afd1a94a8efc39f7de
Mario Steinhoff <steinhoff.mario@gmail.com>
2011-05-02 21:07:49 +0000

    gdata api test

--- fptbot/src/python/test/language/bottest.py
+++ fptbot/src/python/test/language/bottest.py
@@ -4,21 +4,20 @@ Created on Jan 12, 2011
 @author: msteinhoff
 '''
 
+import gdata.calendar.client
 
+def PrintUserCalendars(calendar_client):
+    feed = calendar_client.GetAllCalendarsFeed()
+    print feed.title.text
+    
+    for i, a_calendar in enumerate(feed.entry):
+        print '\t%s. %s' % (i, a_calendar.title.text,)
+        
 
-test = {'abc': 'foobar', 'hitler': 'reich'}
+calendar_client = gdata.calendar.client.CalendarClient(source='foption-fptbot-1.0')
+calendar_client.ssl = True
 
-print "plain"
-for a in test:
-    print a
-    
-print ""
-print "items"
-for a in test.items():
-    print a
-    
-print ""
-print "values"
-for a in test.values():
-    print a
-    
+calendar_client.ClientLogin('fptbot@googlemail.com', 'deinemudderfistedbots1337', calendar_client.source);
+
+PrintUserCalendars(calendar_client)
+
