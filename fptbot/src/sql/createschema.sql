a799b7d5221b5cef673a87ea13f6a828ca94f5c5
Mario Steinhoff <steinhoff.mario@gmail.com>
2011-05-02 20:10:33 +0000

    updated schema for gdata compatibility

--- fptbot/src/sql/createschema.sql
+++ fptbot/src/sql/createschema.sql
@@ -5,7 +5,7 @@
 /*
  * Create CONFIG table with indizes on PACKGE and KEY (PK)
  */
-CREATE TABLE "BOT_CONFIG" (
+CREATE TABLE "CONFIGVALUES" (
 	"PACKAGE"     TEXT NOT NULL,
 	"KEY"         TEXT NOT NULL,
 	"VALUE"       TEXT NOT NULL,
@@ -16,24 +16,25 @@ CREATE TABLE "BOT_CONFIG" (
 /*
  * Create USER table with indizes on ID (PK) and Name
  */
-CREATE TABLE "BOT_USER" (
+CREATE TABLE "USERS" (
 	"ID"          INTEGER NOT NULL PRIMARY KEY ASC AUTOINCREMENT,
 	"NAME"        TEXT    NOT NULL,
 	"PASSWORD"    TEXT    NOT NULL,
 	"PERMISSION"  INTEGER NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS "IDX_NAME"       ON "BOT_USER" ("NAME");
-CREATE INDEX IF NOT EXISTS "IDX_PERMISSION" ON "BOT_USER" ("PERMISSION");
+CREATE INDEX IF NOT EXISTS "IDX_NAME"       ON "USERS" ("NAME");
+CREATE INDEX IF NOT EXISTS "IDX_PERMISSION" ON "USERS" ("PERMISSION");
 
 /*
- * Create NICKS table with indizes on ID (PK) and Name
+ * Create NICKS table with indizes on nickname (PK)
  */
-CREATE TABLE "BOT_NICKS" (
-	"NICKNAME"    TEXT    NOT NULL PRIMARY KEY ASC,
+CREATE TABLE "NICKNAMES" (
+	"NICKNAME"    TEXT    NOT NULL,
 	"USER_ID"     INTEGER NOT NULL,
 	
-	FOREIGN KEY ("USER_ID") REFERENCES "BOT_USER" ("ID")
+	PRIMARY KEY ("NICKNAME"),
+	FOREIGN KEY ("USER_ID") REFERENCES "USERS" ("ID")
 );
 
 /******************************************************************************/
@@ -41,40 +42,49 @@ CREATE TABLE "BOT_NICKS" (
 /******************************************************************************/
 
 /*
- * Create CALENDAR table with indizes on ID (PK), Category, User and Name
+ * Create CALENDARS table with indizes on ID (PK) and name
  */
-CREATE TABLE "CALENDAR_EVENTS" (
-  "ID"          INTEGER NOT NULL PRIMARY KEY ASC AUTOINCREMENT,
-  "CATEGORY_ID" INTEGER NOT NULL,
-  "USER_ID"     INTEGER NULL,
-  "NAME"        TEXT    NOT NULL,
-  "DATE"        TEXT    NOT NULL,
-  "REMINDED"    INTEGER NOT NULL,
+CREATE TABLE "CALENDARS" (
+	"ID"          INTEGER NOT NULL,
+	"NAME"        TEXT    NOT NULL,
+	"TYPE"        INTEGER NOT NULL,
 	
-  FOREIGN KEY ("CATEGORY_ID") REFERENCES "CALENDAR_CATEGORY" ("ID"),
-  FOREIGN KEY ("USER_ID")     REFERENCES "BOT_USER" ("ID")
+	PRIMARY KEY ("ID")
 );
 
-CREATE INDEX IF NOT EXISTS "IDX_CATEGORY" ON "CALENDAR_EVENTS" ("CATEGORY_ID");
-CREATE INDEX IF NOT EXISTS "IDX_USER"     ON "CALENDAR_EVENTS" ("USER_ID");
-CREATE INDEX IF NOT EXISTS "IDX_DATE"     ON "CALENDAR_EVENTS" ("DATE");
+CREATE INDEX IF NOT EXISTS "IDX_NAME" ON "CALENDARS" ("NAME");
 
 /*
- * Create CATEGORY table with indizes on ID (PK), User and Name
+ * Create EVENTS table with indizes on ID (PK) and date
  */
-CREATE TABLE "CALENDAR_CATEGORY" (
-	"ID"      INTEGER NOT NULL PRIMARY KEY ASC AUTOINCREMENT,
-	"USER_ID" INTEGER NULL,
-	"NAME"    TEXT    NOT NULL,
-	"COLOR"   TEXT    NOT NULL,
-	"DEFAULT" INTEGER NOT NULL,
-
-  FOREIGN KEY ("USER_ID") REFERENCES "BOT_USER" ("ID")
+CREATE TABLE "EVENTS" (
+	"ID"          INTEGER NOT NULL,
+	"CALENDAR_ID" INTEGER NOT NULL,
+	"G_ETAG"      TEXT    NULL,
+	"DATE_FROM"   TEXT    NOT NULL,
+	"DATE_TO"     TEXT    NOT NULL,
+	"DESCRIPTION" TEXT    NOT NULL,
+	"CREATED"     TEXT    NOT NULL,
+	"CHANGED"     TEXT    NULL,
+	
+	PRIMARY KEY ("ID"),
+	FOREIGN KEY ("CALENDAR_ID") REFERENCES "CALENDARS" ("ID")
 );
 
-CREATE INDEX IF NOT EXISTS "IDX_USER" ON "CALENDAR_CATEGORY" ("USER_ID");
-CREATE INDEX IF NOT EXISTS "IDX_NAME" ON "CALENDAR_CATEGORY" ("NAME");
+CREATE INDEX IF NOT EXISTS "IDX_DATE" ON "EVENTS" ("DATE_FROM", "DATE_TO");
 
+/*
+ * Create CONTACTS table with indizes on firstname/lastname (PK) and birthday
+ */
+CREATE TABLE "CONTACTS" (
+	"FIRST_NAME"  TEXT    NOT NULL,
+	"LAST_NAME"   TEXT    NOT NULL,
+	"BIRTHDAY"    TEXT    NOT NULL,
+  
+	PRIMARY KEY ("FIRST_NAME", "LAST_NAME")
+);
+
+CREATE INDEX IF NOT EXISTS "IDX_BIRTHDAY" ON "CONTACTS" ("BIRTHDAY");
 
 /******************************************************************************/
 /* facts tables                                                               */
@@ -84,8 +94,10 @@ CREATE INDEX IF NOT EXISTS "IDX_NAME" ON "CALENDAR_CATEGORY" ("NAME");
  * Create FACTS table with indizes on DATE (PK)
  */
 CREATE TABLE "FACTS" (
-	"DATE" TEXT NOT NULL PRIMARY KEY,
-	"TEXT" TEXT NOT NULL
+	"DATE" TEXT NOT NULL,
+	"TEXT" TEXT NOT NULL,
+	
+	PRIMARY KEY ("DATE")
 );
 
 /******************************************************************************/
