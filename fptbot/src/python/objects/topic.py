293f56a3e3fb3547e60708158ba8935fba09648e
Rack Rackowitsch <rackowitsch@gmail.com>
2012-02-14 12:33:01 +0000

    Implemented Topic and TopicAddition.

---
+++ fptbot/src/python/objects/topic.py
@@ -0,0 +1,61 @@
+'''
+Created on 27.01.2012
+
+@author: rack
+'''
+
+__version__ = ''
+
+from sqlalchemy import Column, Integer, DateTime, Text
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.schema import ForeignKey
+from core.persistence import SqlAlchemyPersistence
+
+class Topic(SqlAlchemyPersistence.Base):
+    """
+    Represent an individual topic for the topic component
+    """
+    
+    __tablename__ = 'topics'
+    
+    #DLL
+    id = Column(Integer, primary_key=True)
+    addition_id = Column(Integer, ForeignKey('topicadditions.id'))
+    date = Column(DateTime)
+    text = Column(Text)
+    year = Column(Integer)
+    user = Column(Text)
+    
+    #ORM - one-to-many
+    topicaddition = relationship('TopicAddition', backref=backref('topics'))
+    
+    def __repr__(self):
+        return '<Topic(id={0}|date={1}|text={2}|year={3}|user={4})>'.format(
+            self.id,
+            self.date,
+            self.text,
+            self.year,
+            self.user
+        )
+    
+class TopicAddition(SqlAlchemyPersistence.Base):
+    """
+    Represent an Addition for the topic component
+    """
+    
+    __tablename__ = 'topicadditions'
+    
+    #DLL
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime)
+    text = Column(Text)
+    user = Column(Text)
+       
+    def __repr__(self):
+        return '<TopicAddition(id={0}|date={1}|text={2}|user={3})>'.format(
+            self.id,
+            self.date,
+            self.text,
+            self.user,
+        )
+
